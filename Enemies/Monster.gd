dddde95d933f620d1b3723fc07ced8f4e7755c74
Yuxiao.LEI <yuxiao.lei@groupe-esigelec.org>
2019-10-28 20:42:00 +0800

    unfinished AI

--- Enemies/Monster.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _on_Player_character_attack(attack:Attack):
-	# The player attacked. We need to check here if the current monster is hurt
-	# TODO: the check is just for debug, it needs to be improved
-	if position.x < attack.start_position.x + attack.range_effect:
-		print("Damage done: " + str(attack.damage))
--- Scenes/MainStage.gd
+++ Scenes/MainStage.gd
@@ -1,4 +1,5 @@
 extends Node
 
 func _ready():
+	$Enemies/Monster.add_collision_exception_with($Enemies/Monster2)
 	pass
