823a9b40f4ad962dab292f82aaae9a920b7476ab
Yuxiao.LEI <yuxiao.lei@groupe-esigelec.org>
2020-03-01 08:50:14 +0800

    change scene

--- Elements/ChangeStage.gd
+++ Elements/ChangeStage.gd
@@ -8,7 +8,8 @@ var area_entered = false
 onready var animation_player = $AnimationPlayer
 
 func _ready():
-	pass
+	global.current_scene = target_stage
+	animation_player.play_backwards("fade")
 
 func _on_ChangeStage_body_entered(body):
 	if "Player" in body.name:
@@ -22,11 +23,11 @@ func _on_ChangeStage_body_exited(body):
 	
 func _process(delta):
 	if Input.is_action_pressed("ctrl") and area_entered:
+		if target_stage == null or target_stage == "":
+			var current_stage = str(get_tree().get_current_scene().get_path())
+			var length = current_stage.length()
+			target_stage = "res://Scenes/Stage" + str(current_stage.substr(length-1,length).to_int()+1) + ".tscn"
 		animation_player.play("fade")
 		yield(animation_player,"animation_finished")
 		get_tree().change_scene(target_stage)
-		animation_player.play_backwards("fade")
-		#yield(animation_player,"animation_finished")
-
-
-
+		
